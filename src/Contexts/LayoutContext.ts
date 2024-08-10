8394a7d637a89f8816eaf767c4bbebcf16d23885
Jere <24190408+Dynrothe@users.noreply.github.com>
2024-08-10 15:09:44 +0300

    Layout context changes

--- src/Contexts/LayoutContext.ts
+++ src/Contexts/LayoutContext.ts
@@ -1,9 +1,3 @@
 import { createContext } from "react";
-import Layouts from "../module_bindings/layouts";
 
-export const LayoutContext = createContext<LayoutContextType | undefined>(undefined);
-
-export type LayoutContextType = {
-  activeLayout: Layouts;
-  setActiveLayout: Function;
-};
+export const LayoutContext = createContext<any>(undefined);
