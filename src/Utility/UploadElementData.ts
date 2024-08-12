1be69ba254bd888e8ec675552f0eb6e92f97ab5c
Lethalchip <36650721+Lethalchip@users.noreply.github.com>
2024-08-11 17:22:11 -0700

    improved import/export to support elements and layouts

--- src/Utility/UploadElementData.ts
+++ src/Utility/UploadElementData.ts
@@ -1,4 +1,11 @@
 import AddElementDataReducer from "../module_bindings/add_element_data_reducer";
+import AddElementToLayoutReducer from "../module_bindings/add_element_to_layout_reducer";
+import AddLayoutReducer from "../module_bindings/add_layout_reducer";
+import ElementData from "../module_bindings/element_data";
+import ElementStruct from "../module_bindings/element_struct";
+import Elements from "../module_bindings/elements";
+import ImageElementData from "../module_bindings/image_element_data";
+import Layouts from "../module_bindings/layouts";
 
 export const UploadElementDataFromFile = (backupFile: any) => {
   let reader = new FileReader();
@@ -28,4 +35,92 @@ export const UploadElementDataFromString = (data: string) => {
   elements.forEach((e) => {
     AddElementDataReducer.call(e.name, e.dataType, e.data, e.dataWidth || 128, e.dataHeight || 128);
   });
+};
+
+export const UploadBackupFromFile = (backupFile: any) => {
+  let reader = new FileReader();
+  let data;
+  let error;
+
+  reader.readAsText(backupFile);
+
+  reader.onload = function () {
+    if(!reader.result) return;
+
+    data = reader.result.toString();
+    var backup = JSON.parse(data) as {
+      data: string | null
+      elements: string | null,
+      layouts: string | null
+    };
+
+    let maxLayout = 0;
+    let maxData = 0;
+    Layouts.all().forEach((l) => {
+      if(l.id > maxLayout) maxLayout = l.id;
+    });
+    ElementData.all().forEach((d) => {
+      if(d.id > maxData) maxData = d.id;
+    });
+
+    if(backup.data) {
+      const upData: ElementData[] = JSON.parse(backup.data) as ElementData[];
+      upData.forEach((e) => {
+        AddElementDataReducer.call(e.name, e.dataType, e.data, e.dataWidth || 128, e.dataHeight || 128);
+      })
+    }
+
+    if(backup.layouts) {
+      const upLayouts: Layouts[] = JSON.parse(backup.layouts) as Layouts[];
+      upLayouts.reverse().forEach((e) => {
+        if(e.id!==1 || e.name!=="Default" || e.createdBy!=="Server") AddLayoutReducer.call(e.name, false);
+      })
+    }
+
+    if(backup.elements) {
+      const upElements: Elements[] = JSON.parse(backup.elements) as Elements[];
+      upElements.forEach((e) => {
+        var layoutId = backup.layouts ? e.layoutId + maxLayout - 1 : 1;
+
+        var newElementStruct: ElementStruct;
+
+        switch(e.element.tag) {
+          case "TextElement":
+            newElementStruct = ElementStruct.TextElement({
+              text: e.element.value.text,
+              size: e.element.value.size,
+              color: e.element.value.color,
+              font: e.element.value.font
+            })
+            break;
+          case "ImageElement":
+            var eData: ImageElementData;
+            switch(e.element.value.imageElementData.tag) {
+              case "ElementDataId":
+                eData = ImageElementData.ElementDataId(e.element.value.imageElementData.value + maxData);
+                break;
+              case "RawData":
+                eData = ImageElementData.RawData(e.element.value.imageElementData.value);
+            };
+
+            newElementStruct = ElementStruct.ImageElement({
+              imageElementData: eData,
+              width: e.element.value.width,
+              height: e.element.value.height
+            });
+            break;
+          case "WidgetElement":
+            newElementStruct = ElementStruct.WidgetElement({
+              elementDataId: e.element.value.elementDataId + maxData,
+              width: e.element.value.width,
+              height: e.element.value.height,
+              rawData: e.element.value.rawData
+            });
+            break;
+        };
+
+        AddElementToLayoutReducer.call(newElementStruct,e.transparency,e.transform,e.clip,layoutId);
+      })
+    }
+  }
 };
