9b33efd218c854ae1ac784250a8176f7a5937834
Jere <24190408+Dynrothe@users.noreply.github.com>
2024-09-25 22:02:05 +0300

    Fixed widgets becoming interactable

--- src/Pages/Widget.tsx
+++ src/Pages/Widget.tsx
@@ -1,15 +1,17 @@
 export const WidgetPage = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const b64 = urlParams.get("b64");
-    if(!b64) return(<></>);
-    const parsed = decodeURIComponent(atob(b64));
+  const urlParams = new URLSearchParams(window.location.search);
+  const b64 = urlParams.get("b64");
+  if (!b64) return <></>;
+  const parsed = decodeURIComponent(atob(b64));
 
-    document.body.innerHTML = parsed.substring(parsed.indexOf("<body>"),parsed.indexOf("<script>")-1)+"</body>";
-    document.head.innerHTML = parsed.substring(parsed.indexOf("<head>"),parsed.indexOf("</head>")+7);
+  document.body.style.pointerEvents = "none";
 
-    var script = document.createElement("script");
-    script.innerHTML = parsed.substring(parsed.indexOf("<script>")+8,parsed.indexOf("</script>"));
-    
-    document.head.appendChild(script);
-    return (<></>);
+  document.body.innerHTML = parsed.substring(parsed.indexOf("<body>"), parsed.indexOf("<script>") - 1) + "</body>";
+  document.head.innerHTML = parsed.substring(parsed.indexOf("<head>"), parsed.indexOf("</head>") + 7);
+
+  var script = document.createElement("script");
+  script.innerHTML = parsed.substring(parsed.indexOf("<script>") + 8, parsed.indexOf("</script>"));
+
+  document.head.appendChild(script);
+  return <></>;
 };
