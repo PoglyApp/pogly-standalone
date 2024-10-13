5f3ea46e18215b5fcfc66ba8417640696c762262
Lethalchip <36650721+Lethalchip@users.noreply.github.com>
2024-10-13 16:53:53 -0700

    bindings for perm import fix

--- src/module_bindings/import_permission_reducer.ts
+++ src/module_bindings/import_permission_reducer.ts
@@ -3,43 +3,37 @@
 
 // @ts-ignore
 import { __SPACETIMEDB__, AlgebraicType, ProductType, BuiltinType, ProductTypeElement, DatabaseTable, AlgebraicValue, ReducerArgsAdapter, SumTypeVariant, Serializer, Identity, Address, ReducerEvent, Reducer, SpacetimeDBClient } from "@clockworklabs/spacetimedb-sdk";
-// @ts-ignore
-import { PermissionLevel } from "./permission_level";
 
 export class ImportPermissionReducer extends Reducer
 {
 	public static reducerName: string = "ImportPermission";
-	public static call(_identity: Identity, _permissionLevel: PermissionLevel) {
+	public static call(_identity: Uint8Array, _permissionLevel: string) {
 		this.getReducer().call(_identity, _permissionLevel);
 	}
 
-	public call(_identity: Identity, _permissionLevel: PermissionLevel) {
+	public call(_identity: Uint8Array, _permissionLevel: string) {
 		const serializer = this.client.getSerializer();
-		let _identityType = AlgebraicType.createProductType([
-			new ProductTypeElement("__identity_bytes", AlgebraicType.createArrayType(AlgebraicType.createPrimitiveType(BuiltinType.Type.U8))),
-		]);
+		let _identityType = AlgebraicType.createArrayType(AlgebraicType.createPrimitiveType(BuiltinType.Type.U8));
 		serializer.write(_identityType, _identity);
-		let _permissionLevelType = PermissionLevel.getAlgebraicType();
+		let _permissionLevelType = AlgebraicType.createPrimitiveType(BuiltinType.Type.String);
 		serializer.write(_permissionLevelType, _permissionLevel);
 		this.client.call("ImportPermission", serializer);
 	}
 
 	public static deserializeArgs(adapter: ReducerArgsAdapter): any[] {
-		let identityType = AlgebraicType.createProductType([
-			new ProductTypeElement("__identity_bytes", AlgebraicType.createArrayType(AlgebraicType.createPrimitiveType(BuiltinType.Type.U8))),
-		]);
+		let identityType = AlgebraicType.createArrayType(AlgebraicType.createPrimitiveType(BuiltinType.Type.U8));
 		let identityValue = AlgebraicValue.deserialize(identityType, adapter.next())
-		let identity = new Identity(identityValue.asProductValue().elements[0].asBytes());
-		let permissionLevelType = PermissionLevel.getAlgebraicType();
+		let identity = identityValue.asBytes();
+		let permissionLevelType = AlgebraicType.createPrimitiveType(BuiltinType.Type.String);
 		let permissionLevelValue = AlgebraicValue.deserialize(permissionLevelType, adapter.next())
-		let permissionLevel = PermissionLevel.fromValue(permissionLevelValue);
+		let permissionLevel = permissionLevelValue.asString();
 		return [identity, permissionLevel];
 	}
 
-	public static on(callback: (reducerEvent: ReducerEvent, _identity: Identity, _permissionLevel: PermissionLevel) => void) {
+	public static on(callback: (reducerEvent: ReducerEvent, _identity: Uint8Array, _permissionLevel: string) => void) {
 		this.getReducer().on(callback);
 	}
-	public on(callback: (reducerEvent: ReducerEvent, _identity: Identity, _permissionLevel: PermissionLevel) => void)
+	public on(callback: (reducerEvent: ReducerEvent, _identity: Uint8Array, _permissionLevel: string) => void)
 	{
 		this.client.on("reducer:ImportPermission", callback);
 	}
