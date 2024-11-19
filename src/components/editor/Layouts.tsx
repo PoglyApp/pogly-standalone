e6b8090ff17b86505f6336c640481b7fd5f4182e
Jere <24190408+Dynrothe@users.noreply.github.com>
2024-11-19 21:14:14 +0200

    Add ability to hide layouts and layer menus

--- src/components/editor/Layouts.tsx
+++ src/components/editor/Layouts.tsx
@@ -1,11 +1,55 @@
+import styled from "styled-components";
 import { Container } from "../general/Container";
 import { Dropdown } from "../inputs/Dropdown";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, ChevronLeft } from "lucide-react";
+import { Button } from "../inputs/Button";
+import { useState } from "react";
 
-export const Layouts: React.FC = () => {
+export const Layouts = () => {
+  const [hidden, setHidden] = useState<boolean>(false);
+
+  if (hidden) {
+    return (
+      <HiddenContainer className="enablePointerEvents pt-2 pb-2 mt-14">
+        <Button
+          text="Layouts"
+          fontSize="18px"
+          style={{ textOrientation: "mixed", writingMode: "tb-rl" }}
+          onclick={() => {
+            setHidden(!hidden);
+          }}
+          tooltip="Show"
+        />
+      </HiddenContainer>
+    );
+  }
+
   return (
     <Container className="ml-5 mt-14 enablePointerEvents" style={{ height: "fit-content" }} title="layouts">
-      <Dropdown style={{ width: "300px" }} icon={<LayoutDashboard />} />
+      <div className="flex items-end">
+        <Dropdown style={{ width: "300px" }} icon={<LayoutDashboard />} />
+        <Button
+          icon={<StyledChevronLeft />}
+          onclick={() => {
+            setHidden(!hidden);
+          }}
+          tooltip="Hide"
+        />
+      </div>
     </Container>
   );
 };
+
+const StyledChevronLeft = styled(ChevronLeft)`
+  cursor: pointer;
+  margin-right: -18px;
+  color: #7e828c;
+`;
+
+const HiddenContainer = styled.div`
+  width: 33px;
+  background-color: #1e212b;
+
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+`;
--- src/components/editor/layers/Layers.tsx
+++ src/components/editor/layers/Layers.tsx
@@ -1,15 +1,65 @@
 import { LayerElementType } from "@/types/LayerElementType";
 import { Container } from "../../general/Container";
 import { LayerElement } from "./LayerElement";
+import { useState } from "react";
+import styled from "styled-components";
+import { ChevronLeft } from "lucide-react";
+import { Button } from "@/components/inputs/Button";
 
 export const Layers: React.FC = () => {
+  const [hidden, setHidden] = useState<boolean>(false);
+
+  if (hidden) {
+    return (
+      <HiddenContainer className="enablePointerEvents pt-2 pb-2 mt-14">
+        <Button
+          text="Layers"
+          fontSize="18px"
+          style={{ textOrientation: "mixed", writingMode: "tb-rl" }}
+          onclick={() => {
+            setHidden(!hidden);
+          }}
+          tooltip="Show"
+        />
+      </HiddenContainer>
+    );
+  }
+
   return (
     <Container className="ml-5 mt-8 enablePointerEvents" title="layers">
-      <LayerElement type={LayerElementType.Image} text="Kekw" />
-      <LayerElement type={LayerElementType.Image} text="Pog" />
-      <LayerElement type={LayerElementType.Image} text="Peeposit" />
-      <LayerElement type={LayerElementType.Text} text="Sub now!" />
-      <LayerElement type={LayerElementType.Widget} text="Youtube player" />
+      <div className="flex">
+        <div>
+          <LayerElement type={LayerElementType.Image} text="Kekw" />
+          <LayerElement type={LayerElementType.Image} text="Pog" />
+          <LayerElement type={LayerElementType.Image} text="Peeposit" />
+          <LayerElement type={LayerElementType.Text} text="Sub now!" />
+          <LayerElement type={LayerElementType.Widget} text="Youtube player" />
+        </div>
+
+        <div className="self-center">
+          <Button
+            icon={<StyledChevronLeft />}
+            onclick={() => {
+              setHidden(!hidden);
+            }}
+            tooltip="Hide"
+          />
+        </div>
+      </div>
     </Container>
   );
 };
+
+const StyledChevronLeft = styled(ChevronLeft)`
+  cursor: pointer;
+  margin-right: -18px;
+  color: #7e828c;
+`;
+
+const HiddenContainer = styled.div`
+  width: 33px;
+  background-color: #1e212b;
+
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+`;
