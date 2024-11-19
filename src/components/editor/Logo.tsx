b9551e4274d6d3a9ea5dbb1c9dc0dc29e6414ace
Jere <24190408+Dynrothe@users.noreply.github.com>
2024-11-19 22:04:04 +0200

    Update font

--- src/components/editor/Logo.tsx
+++ src/components/editor/Logo.tsx
@@ -1,8 +1,8 @@
 export const Logo: React.FC = () => {
   return (
-    <div className="flex items-center space-x-2 ml-5 mt-5">
-      <span className="font-semibold text-5xl" style={{ color: "#edf1ff" }}>
-        Pogly <span style={{ color: "#82a5ff" }}>Standalone</span>
+    <div className="flex items-center ml-5 mt-5">
+      <span className=" text-5xl" style={{ color: "#edf1ff" }}>
+        Pogly <span style={{ color: "#82a5ff", marginLeft: "-10px" }}>Standalone</span>
       </span>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import Unfonts from "unplugin-fonts/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: "Geist",
+            src: "./src/assets/fonts/geist/*.woff2",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
