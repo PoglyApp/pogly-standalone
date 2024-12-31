2fd99e1e0efa4e110844901bfc7512e52d75e638
Lethalchip <36650721+Lethalchip@users.noreply.github.com>
2024-12-31 01:51:02 -0800

    deregister one-time-use callbacks after logic

--- src/spacetimedb/hooks/useConnectEvents.tsx
+++ src/spacetimedb/hooks/useConnectEvents.tsx
@@ -1,32 +1,43 @@
-import { DBConnection } from "@/module_bindings";
+import { DBConnection, EventContext, Guests } from "@/module_bindings";
 
 export const useConnectEvents = (stdb: DBConnection, authKey: string, setAuthentication: Function) => {
 
-    stdb.onReducer("Connect", (ctx: any) => {
-        const configSettings = stdb.db.config.version.find(0);
-        const localGuest = stdb.db.guests.address.find(stdb.clientAddress);
-        if (
-            ctx.clientAddress !== stdb.clientAddress || 
-            !configSettings || 
-            !localGuest
-        ) return;
+    const connectCallback = (ctx: any) => {
+        try {
+            const configSettings = stdb.db.config.version.find(0);
+            const localGuest = stdb.db.guests.address.find(stdb.clientAddress);
+            if (
+                ctx.clientAddress !== stdb.clientAddress || 
+                !configSettings || 
+                !localGuest
+            ) return;
+    
+            if(configSettings.authentication && !localGuest.authenticated) stdb.reducers.authenticate(authKey);
+        } finally {
+            stdb.offReducer("Connect", connectCallback);
+        }
+    };
 
-        if(configSettings.authentication && !localGuest.authenticated) stdb.reducers.authenticate(authKey);
-    });
+    const authenticateCallback = (ctx: EventContext, oldGuest: Guests, newGuest: Guests) => {
+        try {
+            const configSettings = stdb.db.config.version.find(0);
+            const localGuest = stdb.db.guests.address.find(stdb.clientAddress);
+    
+            if (
+                !configSettings || 
+                !localGuest || 
+                ctx.event.tag !== "Reducer" || 
+                ctx.event.value.reducer.name !== "Authenticate" || 
+                ctx.event.value.status.tag !== "Committed" || 
+                oldGuest.address.toHexString() !== stdb.clientAddress.toHexString()
+            ) return;
+            
+            if(!oldGuest.authenticated && newGuest.authenticated) setAuthentication(true);
+        } finally {
+            stdb.db.guests.removeOnUpdate(authenticateCallback);
+        }
+    };
 
-    stdb.db.guests.onUpdate((ctx, oldGuest, newGuest) => {
-        const configSettings = stdb.db.config.version.find(0);
-        const localGuest = stdb.db.guests.address.find(stdb.clientAddress);
-
-        if (
-            !configSettings || 
-            !localGuest || 
-            ctx.event.tag !== "Reducer" || 
-            ctx.event.value.reducer.name !== "Authenticate" || 
-            ctx.event.value.status.tag !== "Committed" || 
-            oldGuest.address.toHexString() !== stdb.clientAddress.toHexString()
-        ) return;
-        
-        if(!oldGuest.authenticated && newGuest.authenticated) setAuthentication(true);
-    });
+    stdb.onReducer("Connect", connectCallback);
+    stdb.db.guests.onUpdate(authenticateCallback);
 }
