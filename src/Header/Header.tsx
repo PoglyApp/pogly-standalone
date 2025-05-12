a5348cfa88980263e83022ce65ca7e4bbc5bb2d6
Jere <24190408+Dynrothe@users.noreply.github.com>
2025-05-12 20:45:19 +0300

    Fix crash while trying to access canvas route when unauthenticated

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { AppBar, Box, MenuItem, Tab, Tabs, Typography } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
 import { GuestListContainer } from "../Components/Containers/GuestListContainer";
 import { SettingsModal } from "../Components/Modals/SettingModals";
 import SecurityIcon from "@mui/icons-material/Security";
@@ -15,23 +15,25 @@ import { HandleDragAndDropFiles } from "../Utility/HandleDragAndDropFiles";
 import { ElementSelectionMenu } from "../Components/ElementSelectionMenu/ElementSelectionMenu";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import { QuickSwapMenu } from "./QuickswapMenu";
+import { SpacetimeContext } from "../Contexts/SpacetimeContext";
 
 export const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
+
   const { setModals } = useContext(ModalContext);
+
+  const { spacetimeDB } = useContext(SpacetimeContext);
 
   const [isRedirecting, setIsRedirecting] = useState(false);
   const [isDroppingSelectionMenu, setIsDroppingSelectionMenu] = useState(false);
   const [quickSwapMenuAnchor, setQuickSwapMenuAnchor] = useState<any>(null);
   const quickSwapMenuOpen = Boolean(quickSwapMenuAnchor);
 
+  useEffect(() => {}, []);
+
   useEffect(() => {
-    if (
-      location.pathname !== "/login" &&
-      location.pathname !== "/" &&
-      !location.pathname.startsWith("/canvas")
-    ) {
+    if (location.pathname !== "/login" && location.pathname !== "/" && !location.pathname.startsWith("/canvas")) {
       setIsRedirecting(true);
       window.location.href = "/";
     }
@@ -47,92 +49,87 @@ export const Header = () => {
     setModals((oldModals: any) => [...oldModals, <EditorGuidelineModal key="guideline_modal" />]);
   };
 
-  return isRedirecting ? null : (
-    location.pathname !== "/canvas" ? (
+  if (location.pathname === "/canvas" && !spacetimeDB) return <Navigate to="/login" replace />;
+
+  return isRedirecting ? null : location.pathname !== "/canvas" ? (
+    <main>
+      <Outlet />
+    </main>
+  ) : (
+    <>
+      <StyledBox>
+        <AppBar position="static">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: "monospace",
+                fontWeight: 700,
+                color: "#ffffffa6",
+                minWidth: "218px",
+                textAlign: "center",
+              }}
+            >
+              <span style={{ color: "#ffffffd9" }}>Pogly</span> Standalone
+            </Typography>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Tabs
+                value={0}
+                sx={{
+                  ".Mui-selected": { backgroundColor: "#001529 !important" },
+                  ".MuiTabs-indicator": { backgroundColor: "#001529 !important" },
+                }}
+              >
+                <StyledTab
+                  id="quickswap-button"
+                  aria-controls={quickSwapMenuOpen ? "quickswap-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={quickSwapMenuOpen ? "true" : undefined}
+                  icon={<SwapHorizIcon />}
+                  iconPosition="start"
+                  label="Swap module"
+                  onClick={(event: any) => setQuickSwapMenuAnchor(event.currentTarget)}
+                />
+                <StyledTab icon={<SettingsIcon />} iconPosition="start" label="Settings" onClick={showSettingsMenu} />
+                <StyledGuidelines
+                  icon={<SecurityIcon />}
+                  iconPosition="start"
+                  label="Editor Guidelines"
+                  onClick={showEditorGuidelines}
+                />
+              </Tabs>
+            </Box>
+
+            <GuestListContainer />
+          </Toolbar>
+        </AppBar>
+
+        <Dropzone
+          onDrop={(acceptedFiles) => HandleDragAndDropFiles(acceptedFiles, setModals)}
+          noClick={true}
+          onDragEnter={() => setIsDroppingSelectionMenu(true)}
+          onDragLeave={() => setIsDroppingSelectionMenu(false)}
+          onDropAccepted={() => setIsDroppingSelectionMenu(false)}
+          onDropRejected={() => setIsDroppingSelectionMenu(false)}
+        >
+          {({ getRootProps }) => (
+            <div {...getRootProps()}>
+              <ElementSelectionMenu isDropping={isDroppingSelectionMenu} />
+            </div>
+          )}
+        </Dropzone>
+
+        <QuickSwapMenu
+          quickSwapMenuAnchor={quickSwapMenuAnchor}
+          quickSwapMenuOpen={quickSwapMenuOpen}
+          setQuickSwapMenuAnchor={setQuickSwapMenuAnchor}
+        />
+      </StyledBox>
+
       <main>
         <Outlet />
       </main>
-    ) : (
-      <>
-        <StyledBox>
-          <AppBar position="static">
-            <Toolbar disableGutters>
-              <Typography
-                variant="h6"
-                sx={{
-                  fontFamily: "monospace",
-                  fontWeight: 700,
-                  color: "#ffffffa6",
-                  minWidth: "218px",
-                  textAlign: "center",
-                }}
-              >
-                <span style={{ color: "#ffffffd9" }}>Pogly</span> Standalone
-              </Typography>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <Tabs
-                  value={0}
-                  sx={{
-                    ".Mui-selected": { backgroundColor: "#001529 !important" },
-                    ".MuiTabs-indicator": { backgroundColor: "#001529 !important" },
-                  }}
-                >
-                  <StyledTab
-                    id="quickswap-button"
-                    aria-controls={quickSwapMenuOpen ? "quickswap-menu" : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={quickSwapMenuOpen ? "true" : undefined}
-                    icon={<SwapHorizIcon />}
-                    iconPosition="start"
-                    label="Swap module"
-                    onClick={(event: any) => setQuickSwapMenuAnchor(event.currentTarget)}
-                  />
-                  <StyledTab
-                    icon={<SettingsIcon />}
-                    iconPosition="start"
-                    label="Settings"
-                    onClick={showSettingsMenu}
-                  />
-                  <StyledGuidelines
-                    icon={<SecurityIcon />}
-                    iconPosition="start"
-                    label="Editor Guidelines"
-                    onClick={showEditorGuidelines}
-                  />
-                </Tabs>
-              </Box>
-
-              <GuestListContainer />
-            </Toolbar>
-          </AppBar>
-
-          <Dropzone
-            onDrop={(acceptedFiles) => HandleDragAndDropFiles(acceptedFiles, setModals)}
-            noClick={true}
-            onDragEnter={() => setIsDroppingSelectionMenu(true)}
-            onDragLeave={() => setIsDroppingSelectionMenu(false)}
-            onDropAccepted={() => setIsDroppingSelectionMenu(false)}
-            onDropRejected={() => setIsDroppingSelectionMenu(false)}
-          >
-            {({ getRootProps }) => (
-              <div {...getRootProps()}>
-                <ElementSelectionMenu isDropping={isDroppingSelectionMenu} />
-              </div>
-            )}
-          </Dropzone>
-
-          <QuickSwapMenu
-            quickSwapMenuAnchor={quickSwapMenuAnchor}
-            quickSwapMenuOpen={quickSwapMenuOpen}
-            setQuickSwapMenuAnchor={setQuickSwapMenuAnchor}
-          />
-        </StyledBox>
-
-        <main>
-          <Outlet />
-        </main>
-      </>
-    )
+    </>
   );
 };
 
