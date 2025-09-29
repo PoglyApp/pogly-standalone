9140384edaa4fc57f14ce4df156a13f9c132cae4
Chippy <36650721+Lethalchip@users.noreply.github.com>
2025-09-28 17:36:07 -0700

    update spacetimeauth staging

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,12 @@ import { AuthProvider, useAuth } from "react-oidc-context";
 
 const oidcConfig = {
   authority: "https://spacetimeauth.staging.spacetimedb.com/oidc",
-  client_id: "client_030t5fnI0zvgj3KScXWDp8",
+  client_id: "client_0319cWpFKkEcwmVeoN44hS",
   redirect_uri: `${window.location.origin}/callback`,
   scope: "openid",
   response_type: "code",
   automaticSilentRenew: false,
-  loadUserInfo: true,
+  loadUserInfo: false,
     metadata: {
     issuer: "https://spacetimeauth.staging.spacetimedb.com/oidc",
     authorization_endpoint: "https://spacetimeauth.staging.spacetimedb.com/oidc/auth",
@@ -75,7 +75,7 @@ export function OidcDebug() {
 }
 
 // Enable/disable oidc debugging
-const oidcDebugLogs = false;
+const oidcDebugLogs = true;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
