ee4b28d59d53324fe8f0e125566c2e11c77f3f3a
Chippy <36650721+Lethalchip@users.noreply.github.com>
2025-09-30 13:40:52 -0700

    update spacetimeauth to production

--- src/index.tsx
+++ src/index.tsx
@@ -8,22 +8,25 @@ import { Provider } from "react-redux";
 import { store } from "./Store/Features/store";
 import { AuthProvider, useAuth } from "react-oidc-context";
 
+//https://auth.spacetimedb.com/oidc/.well-known/openid-configuration
+
 const oidcConfig = {
-  authority: "https://spacetimeauth.staging.spacetimedb.com/oidc",
-  client_id: "client_0319cWpFKkEcwmVeoN44hS",
+  authority: "https://auth.spacetimedb.com/oidc",
+  client_id: "client_031BvnxblLKmMtctMbLllZ",
   redirect_uri: `${window.location.origin}/callback`,
   scope: "openid profile",
   response_type: "code",
   automaticSilentRenew: false,
   loadUserInfo: false,
     metadata: {
-    issuer: "https://spacetimeauth.staging.spacetimedb.com/oidc",
-    authorization_endpoint: "https://spacetimeauth.staging.spacetimedb.com/oidc/auth",
-    token_endpoint:         "https://spacetimeauth.staging.spacetimedb.com/oidc/token",
-    jwks_uri:               "https://spacetimeauth.staging.spacetimedb.com/oidc/jwks",
-    end_session_endpoint:   "https://spacetimeauth.staging.spacetimedb.com/oidc/session/end",
-    pushed_authorization_request_endpoing: "https://spacetimeauth.staging.spacetimedb.com/oidc/request",
-    userinfo_endpoint:      "https://spacetimeauth.staging.spacetimedb.com/oidc/me"
+    issuer: "https://auth.spacetimedb.com/oidc",
+    authorization_endpoint: "https://auth.spacetimedb.com/oidc/auth",
+    token_endpoint:         "https://auth.spacetimedb.com/oidc/token",
+    jwks_uri:               "https://auth.spacetimedb.com/oidc/jwks",
+    end_session_endpoint:   "https://auth.spacetimedb.com/oidc/session/end",
+    pushed_authorization_request_endpoing: "https://auth.spacetimedb.com/oidc/request",
+    userinfo_endpoint:      "https://auth.spacetimedb.com/oidc/me",
+    introspection_endpoint: "https://auth.spacetimedb.com/oidc/token/introspection"
   },
 };
 
@@ -75,7 +78,7 @@ export function OidcDebug() {
 }
 
 // Enable/disable oidc debugging
-const oidcDebugLogs = true;
+const oidcDebugLogs = false;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
