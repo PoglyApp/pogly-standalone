336a5348d7e89e4594dd4206fc442579b9c52494
Jere <24190408+Dynrothe@users.noreply.github.com>
2025-10-26 01:10:52 +0300

    Make Pogly logo responsive

--- src/Components/General/PoglyLogo.tsx
+++ src/Components/General/PoglyLogo.tsx
@@ -1,17 +1,11 @@
 export const PoglyLogo: React.FC = () => {
   return (
-    <div
-      className="flex space-x-2 ml-5 mt-5 mb-1 font-geist-mono justify-center items-end"
-      style={{ marginTop: "6vh" }}
-    >
-      <span className="font-semibold text-5xl">
-        Pogly <span style={{ color: "#82a5ff" }}>Standalone</span>
+    <div className="flex space-x-2 ml-5 mt-[6vh] mb-1 font-geist-mono justify-center items-end">
+      <span className="font-semibold text-5xl max-sm:w-[290px]">
+        Pogly <span className="text-[#82a5ff]">Standalone</span>
       </span>
 
-      <span
-        className="text-xs font-semibold py-0.2 px-1.5 rounded-md"
-        style={{ backgroundColor: "#82a5ff", color: "#10121a", marginBottom: "3px" }}
-      >
+      <span className="text-xs font-semibold py-0.2 px-1.5 rounded-md bg-[#82a5ff] text-[#10121a] mb-[3px]">
         v{process.env.REACT_APP_VERSION}
       </span>
     </div>
